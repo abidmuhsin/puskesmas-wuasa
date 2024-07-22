ff7390be5b29683601a3899d8ea63c698edbe19b
Abid <abidmhsn@gmail.com>
2024-07-22 18:34:52 +0800

    update

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,10 @@
 import AutoSlider from '../container/AutoSlider.jsx';
-import content from '../assets/dashboard-content.png'
+import content from '../assets/dashboard-content2.png'
+import { Link } from 'react-router-dom'
 
 const Dashboard =()=> {
     return (
-        <div className='relative max-w-full min-h-[75vh] bg-center bg-cover'>
+        <div className='relative max-w-full h-[170vh] bg-center bg-cover'>
             <AutoSlider/>
             {/* <div className="absolute inset-0 bg-gradient-to-r from-blue-800 to-cyan-500  opacity-60"></div>
             <div className='z-10 relative flex flex-col justify-end items-center h-[40vh] text-white gap-4'>
@@ -14,14 +15,15 @@ const Dashboard =()=> {
                 <button className='w-40 h-10 bg-blue-800 hover:bg-blue-950 rounded-md'>Kontak</button>
                 <button className='w-40 h-10 text-black bg-white rounded-md hover:bg-slate-600 hover:text-white'>Pelajari</button>
             </div> */}
-            <div className='max-w-full min-h-[70vh] flex'>
-                <div className='w=[50%] h-[70vh] flex flex-col justify-center items-left gap-5 px-20 '>
+            <div className='max-w-full min-h-[100vh] flex justify-left items-center'>
+                <div className='h-[70vh] flex flex-col justify-center items-left gap-4 px-20 '>
                     <h1 className='text-3xl text-blue-900 font-bold '>Puskesmas Lore Utara Desa Wuasa</h1>
                     <p className=''>Puskesmas Lore Utara adalah pusat kesehatan masyarakat yang berada di sekitar daerah desa wuasa</p>
-                    <button className='flex items-left bg-blue-600 rounded-full w-fit px-8 py-3 text-white'>Selengkapnya</button>
+                    <button className='flex items-left bg-blue-600 rounded-full w-fit px-8 py-3 text-white'><Link to="/aboutus">Selengkapnya</Link></button>
                 </div>
-                <div className='w-[50vh] h-[50vh] flex justify-center items-center'>
-                    <img src={content} alt="" />
+                <div className='w-[50vh] h-[75vh] flex flex-row'>
+                    <img src={content} alt="" className='pt-20 bg-blue-500 rounded-tl-[15vh] border border-blue-500 border-4 absolute top-[90vh] right-15 w-[50vh] z-10'/>
+                    <div className='relative w-96 h-70 top-[10vh] right-[-10vh] bg-gradient-to-t from-blue-500 to-blue-400 rounded-br-[15vh] z-0'></div>
                 </div>
             </div>
            
